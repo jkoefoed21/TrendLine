e8b0ed4b424b543bf186aba6798513b01a8c85c5
Jack Koefoed <jackkoefoed@hotmail.com>
2016-10-20 15:05:02 -0700

    A work in progress

--- BestFitLine/BestFitLine/BestFit.cs
+++ BestFitLine/BestFitLine/BestFit.cs
@@ -3,13 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BestFitLine
 {
-    class Program
+    class BestFit
     {
+        public static readonly int DIMENSION = 3;
+
+        public static String filePath = "C:\\Users\\Jack Koefoed\\OneDrive\\12\\Multi\\bestfit_dataset_example.txt";
+
         static void Main(string[] args)
         {
+            List<double> xValues = new List<double>();
+            List<double> yValues = new List<double>();
+            String[] lines = File.ReadAllLines(filePath);
+            foreach (String line in lines)
+            {
+                String[] vals = line.Split('\t');
+                for(int ii=0; ii<vals.Length; ii++)
+                {
+                    if (ii%2==0)
+                    {
+                        xValues.Add(double.Parse(vals[ii]));
+                    }
+                    else
+                    {
+                        yValues.Add(double.Parse(vals[ii]));
+                    }
+                }
+            }
+            double[] xVals = xValues.ToArray();
+            double[] yVals = yValues.ToArray();
+
+            double[,] origMatrix = new double[DIMENSION, DIMENSION];
+            for (int ii=0; ii<origMatrix.GetLength(0); ii++)
+            {
+                for (int jj = 0; jj < origMatrix.GetLength(1); jj++)
+                {
+                    origMatrix[ii, jj] = mean(xVals, DIMENSION - 1 + ii - jj);
+                }
+            }
+            origMatrix=Matrix.invert(origMatrix);
+
+            double[,] yMatrix = new double[DIMENSION, 1];
+
+            for (int ii=0; ii<yMatrix.GetLength(0); ii++)
+            {
+                yMatrix[ii, 0] = mean(yVals, xVals, 1, ii);
+            }
+            Console.WriteLine("Orig 2");
+            Matrix.print(origMatrix);
+            Console.WriteLine();
+            Matrix.print(yMatrix);
+
+            double[,] result=Matrix.multiply(origMatrix, yMatrix);
+
+            for (int ii=0; ii<result.GetLength(0); ii++)
+            {
+                Console.WriteLine(result[ii, 0]);
+            }
+            /*
+            double[,] lol = { { 1, 3, 1 , 5 }, { 2, 1, 0 , 6 },{ 3, 2, 1 , 7 },{ 3, 5, 2, 7 } };
+            double[,] lmao = { { 1 }, { 2 }, { 4 }, { 3 } };
+            double[,] rofl = { { 2, 3 }, { 5, 6 } };
+            //Matrix.invert(lol);
+            Matrix.print(Matrix.multiply(lol, lmao));
+            Console.ReadKey();*/
+            Console.ReadKey();
+        }
+        
+        public static double mean(double[] nums, int power)
+        {
+            double total = 0;
+            foreach(double num in nums)
+            {
+                total += Math.Pow(num, power);
+            }
+            return total / nums.Length;
+        }
+
+        public static double mean(double[] nums1, double[] nums2, int power1, int power2)
+        {
+            if (nums1.Length!=nums2.Length)
+            {
+                throw new ArgumentException("Lists not same size.");
+            }
+            double total = 0;
+            for (int ii=0; ii<nums1.Length; ii++)
+            {
+                total += (Math.Pow(nums1[ii], power1) * Math.Pow(nums2[ii], power2));
+            }
+            return total / nums1.Length;
         }
     }
 }
--- BestFitLine/BestFitLine/Matrix.cs
+++ BestFitLine/BestFitLine/Matrix.cs
@@ -6,7 +6,162 @@ using System.Threading.Tasks;
 
 namespace BestFitLine
 {
-    class Matrix
+    static class Matrix
     {
+        public static double[,] invert (double[,] mat)
+        {
+            //print(mat);
+            //print(copyAround(mat, 1, 2));
+            //Console.WriteLine(determinant(copyAround(mat, 1, 2)));
+            double det = 1.0/determinant(mat);
+            //Console.WriteLine(det);
+            double[,] oldmat = mat;
+            mat = new double[oldmat.GetLength(0), oldmat.GetLength(1)];
+            for (int ii = 0; ii < mat.GetLength(0); ii++)
+            {
+                for (int jj = 0; jj < mat.GetLength(1); jj++)
+                {
+                    mat[ii,jj] = determinant(copyAround(oldmat, ii, jj));
+                }
+            }
+            //print(mat);
+            transpose(mat);
+            //print(mat);
+            adjunct(mat);
+            //print(mat);
+            multiply(mat, det);
+            Console.WriteLine("Orig 1");
+            print(mat);
+            return mat;
+        }
+
+
+        public static double determinant (double[,] mat)
+        {
+            if (mat.GetLength(0)!=mat.GetLength(1))
+            {
+                throw new ArgumentException();
+            }
+            if (mat.GetLength(0)==2)
+            {
+                return mat[0,0] * mat[1,1] - mat[0,1] * mat[1,0];
+            }
+            else
+            {
+                double total = 0;
+                for (int ii=0; ii<mat.GetLength(1); ii++)
+                {
+                    double val = Math.Pow(-1, ii) * mat[0,ii]*determinant(copyAround(mat, 0, ii));
+                    total += val;
+                }
+                return total;
+            }
+        }
+
+        public static double determinant (double[,] mat, double row, double col)
+        {
+            return determinant(copyAround(mat, row, col));
+        }
+
+        public static void transpose (double[,] mat)
+        {
+            double[,] newMat = new double[mat.GetLength(0),mat.GetLength(1)];
+            for (int ii=0; ii<mat.GetLength(0); ii++)
+            {
+                for (int jj=0; jj<mat.GetLength(1); jj++)
+                {
+                    newMat[ii,jj] = mat[jj,ii];
+                }
+            }
+            mat = newMat;
+        }
+
+        public static void adjunct (double [,] mat)
+        {
+            for (int ii = 0; ii < mat.GetLength(0); ii++)
+            {
+                for (int jj = 0; jj < mat.GetLength(1); jj++)
+                {
+                    mat[ii,jj] = Math.Pow(-1, (ii+ jj)%2)*mat[ii,jj];
+                }
+            }
+        }
+
+        public static void multiply (double[,] mat, double value)
+        {
+            for (int ii = 0; ii < mat.GetLength(0); ii++)
+            {
+                for (int jj = 0; jj < mat.GetLength(1); jj++)
+                {
+                    mat[ii,jj] = mat[ii,jj]*value;
+                }
+            }
+        }
+
+        public static double[,] copyAround (double[,] mat, double row, double col) //working
+        {
+            /*
+            for (int ii=0; ii<mat.GetLength(); ii++)
+            {
+                for (int jj=0; jj<mat.GetLength(); jj++)
+                {
+                    Console.Write(mat[ii,jj]+" ");
+                }
+                Console.WriteLine();
+            }*/
+            double[,] newMat = new double[mat.GetLength(0)-1, mat.GetLength(1)-1];
+
+            for (int ii=0; ii<newMat.GetLength(0); ii++)
+            {
+                for (int jj=0; jj<newMat.GetLength(1); jj++)
+                {
+                    newMat[ii,jj] = mat[ii < row ? ii : ii + 1,jj<col ? jj :jj+1];
+                }
+            }
+            /*
+            for (int ii = 0; ii < newMat.GetLength(); ii++)
+            {
+                for (int jj = 0; jj < newMat.GetLength(); jj++)
+                {
+                    Console.Write(newMat[ii,jj] + " ");
+                }
+                Console.WriteLine();
+            }*/
+            return newMat;
+        }
+        public static void print(double[,] mat)
+        {
+            for (int ii = 0; ii < mat.GetLength(0); ii++)
+            {
+                for (int jj = 0; jj < mat.GetLength(1); jj++)
+                {
+                    Console.Write(mat[ii,jj] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static double[,] multiply(double[,] mat1, double[,] mat2)
+        {
+            if (mat1.GetLength(1)!=mat2.GetLength(0))
+            {
+                Console.WriteLine(mat1.GetLength(0) + " " + mat2.GetLength(1));
+                throw new ArgumentException();
+            }
+            double[,] result = new double[mat1.GetLength(0), mat2.GetLength(1)];
+            for (int ii=0; ii<mat1.GetLength(0); ii++)
+            {
+                for (int jj=0; jj<mat2.GetLength(1); jj++)
+                {
+                    double total = 0;
+                    for (int kk=0; kk<mat1.GetLength(1); kk++)
+                    {
+                        total += mat1[ii, kk] * mat2[kk, jj];
+                    }
+                    result[ii, jj] = total;
+                }
+            }
+            return result;
+        }
     }
 }
