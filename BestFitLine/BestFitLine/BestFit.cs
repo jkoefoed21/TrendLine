873ba42a42bb04cefd8da128add5f5c2ddf9e8b4
Jack Koefoed <jackkoefoed@hotmail.com>
2016-10-20 20:34:38 -0700

    Jagged Arrays--Much faster

--- BestFitLine/BestFitLine/BestFit.cs
+++ BestFitLine/BestFitLine/BestFit.cs
@@ -10,14 +10,14 @@ namespace BestFitLine
 {
     class BestFit
     {
-        public static readonly int ORDER = 11; //this is actual highest x power
+        public static readonly int ORDER = 10; 
 
         public static String filePath = "C:\\Users\\Jack Koefoed\\OneDrive\\12\\Multi\\bestfit_dataset_example.txt";
 
         static void Main(String[] args)
         {
 
-            /* double[,] origMat = { { 1, 3, 1.92 , 5 }, { 2, 1, 0.98 , 6 },{ 3, 2, 1.24 , 7 },{ 3, 5, 2.98 , 7 } };
+            /* double[][] origMat = { { 1, 3, 1.92 , 5 }, { 2, 1, 0.98 , 6 },{ 3, 2, 1.24 , 7 },{ 3, 5, 2.98 , 7 } };
              Matrix.print(origMat);
              //origMat =Matrix.invert(origMat);
              Matrix.print(origMat);
@@ -58,24 +58,26 @@ namespace BestFitLine
             double[] xVals = xValues.ToArray();
             double[] yVals = yValues.ToArray();
 
-            double[,] origMatrix = new double[ORDER, ORDER];
-            for (int ii=0; ii<origMatrix.GetLength(0); ii++)
+            double[][] origMatrix = new double[ORDER][];
+            for (int ii=0; ii<origMatrix.Length; ii++)
             {
-                for (int jj = 0; jj < origMatrix.GetLength(1); jj++)
+                origMatrix[ii] = new double[ORDER];
+                for (int jj = 0; jj < origMatrix[ii].Length; jj++)
                 {
-                    origMatrix[ii, jj] = mean(xVals, ORDER - 1 + ii - jj);
+                    origMatrix[ii][jj] = mean(xVals, ORDER - 1 + ii - jj);
                 }
             }
-            double[,] newMat = (double[,])origMatrix.Clone();
+            double[][] newMat = (double[][])origMatrix.Clone();
 
             //Matrix.print(newMat);
             origMatrix =Matrix.invert(origMatrix);
 
-            double[,] yMatrix = new double[ORDER, 1];
+            double[][] yMatrix = new double[ORDER][];
 
-            for (int ii=0; ii<yMatrix.GetLength(0); ii++)
+            for (int ii=0; ii<yMatrix.Length; ii++)
             {
-                yMatrix[ii, 0] = mean(yVals, xVals, 1, ii);
+                yMatrix[ii] = new double[1];
+                yMatrix[ii][0] = mean(yVals, xVals, 1, ii);
             }
             //Console.WriteLine("Orig 2");
             //Matrix.print(origMatrix);
@@ -86,17 +88,17 @@ namespace BestFitLine
 
             //Matrix.print(yMatrix);
 
-            double[,] result=Matrix.multiply(origMatrix, yMatrix);
+            double[][] result=Matrix.multiply(origMatrix, yMatrix);
 
             Console.WriteLine("Values:");
-            for (int ii=0; ii<result.GetLength(0); ii++)
+            for (int ii=0; ii<result.Length; ii++)
             {
-                Console.WriteLine(result[ii, 0]);
+                Console.WriteLine(result[ii][0]);
             }
             /*
-            double[,] lol = { { 1, 3, 1 , 5 }, { 2, 1, 0 , 6 },{ 3, 2, 1 , 7 },{ 3, 5, 2, 7 } };
-            double[,] lmao = { { 1 }, { 2 }, { 4 }, { 3 } };
-            double[,] rofl = { { 2, 3 }, { 5, 6 } };
+            double[][] lol = { { 1, 3, 1 , 5 }, { 2, 1, 0 , 6 },{ 3, 2, 1 , 7 },{ 3, 5, 2, 7 } };
+            double[][] lmao = { { 1 }, { 2 }, { 4 }, { 3 } };
+            double[][] rofl = { { 2, 3 }, { 5, 6 } };
             //Matrix.invert(lol);
             Matrix.print(Matrix.multiply(lol, lmao));
             Console.ReadKey();*/
--- BestFitLine/BestFitLine/Matrix.cs
+++ BestFitLine/BestFitLine/Matrix.cs
@@ -8,20 +8,21 @@ namespace BestFitLine
 {
     static class Matrix
     {
-        public static double[,] invert (double[,] mat) //Major problem here
+        public static double[][] invert (double[][] mat) //Major problem here
         {
             //print(mat);
             //print(copyAround(mat, 1, 2));
             //Console.WriteLine(determinant(copyAround(mat, 1, 2)));
             double det = 1.0/determinant(mat);
             //Console.WriteLine(det);
-            double[,] oldmat = mat;
-            mat = new double[oldmat.GetLength(0), oldmat.GetLength(1)];
-            for (int ii = 0; ii < mat.GetLength(0); ii++)
+            double[][] oldmat = mat;
+            mat = new double[oldmat.Length][];
+            for (int ii = 0; ii < mat.Length; ii++)
             {
-                for (int jj = 0; jj < mat.GetLength(1); jj++)
+                mat[ii] = new double[oldmat[ii].Length];
+                for (int jj = 0; jj < mat[ii].Length; jj++)
                 {
-                    mat[ii,jj] = determinant(copyAround(oldmat, ii, jj));
+                    mat[ii][jj] = determinant(copyAround(oldmat, ii, jj));
                 }
             }
             //print(mat);
@@ -30,145 +31,130 @@ namespace BestFitLine
             adjunct(mat);
             //print(mat);
             multiply(mat, det);
-            //Console.WriteLine("Orig 1");
+            Console.WriteLine("Orig 1");
             //print(mat);
             return mat;
         }
 
 
-        public static double determinant (double[,] mat)
+        public static double determinant (double[][] mat)
         {
-            if (mat.GetLength(0)!=mat.GetLength(1))
+            if (mat.Length!=mat[0].Length) //not safe but is a compromise between safety and speed
             {
                 throw new ArgumentException();
             }
-            if (mat.GetLength(0)==1)
+            if (mat.Length==1)
             {
-                return mat[0, 0];
+                return mat[0][ 0];
             }
-            if (mat.GetLength(0)==2)
+            if (mat.Length==2)
             {
-                return mat[0,0] * mat[1,1] - mat[0,1] * mat[1,0];
+                return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
             }
             else
             {
                 double total = 0;
-                for (int ii=0; ii<mat.GetLength(1); ii++)
+                for (int ii=0; ii<mat[0].Length; ii++)
                 {
-                    double val = Math.Pow(-1, ii) * mat[0,ii]*determinant(copyAround(mat, 0, ii));
+                    double val = Math.Pow(-1, ii) * mat[0][ii]*determinant(copyAround(mat, 0, ii));
                     total += val;
                 }
                 return total;
             }
         }
 
-        public static double determinant (double[,] mat, double row, double col)
+        public static double determinant (double[][] mat, double row, double col)
         {
             return determinant(copyAround(mat, row, col));
         }
 
-        public static void transpose (double[,] mat)
+        public static void transpose (double[][] mat)
         {
-            double[,] newMat = new double[mat.GetLength(0),mat.GetLength(1)];
-            for (int ii=0; ii<mat.GetLength(0); ii++)
+            double[][] newMat = new double[mat.Length][];
+            for (int ii=0; ii<mat.Length; ii++)
             {
-                for (int jj=0; jj<mat.GetLength(1); jj++)
+                newMat[ii] = new double[mat[ii].Length];
+                for (int jj=0; jj<mat[ii].Length; jj++)
                 {
-                    newMat[ii,jj] = mat[jj,ii];
+                    newMat[ii][jj] = mat[jj][ii];
                 }
             }
-            for (int ii = 0; ii < mat.GetLength(0); ii++)
+            for (int ii = 0; ii < mat.Length; ii++)
             {
-                for (int jj = 0; jj < mat.GetLength(1); jj++)
+                for (int jj = 0; jj < mat[ii].Length; jj++)
                 {
-                    mat[ii, jj] = newMat[ii, jj];
+                    mat[ii][ jj] = newMat[ii][ jj];
                 }
             }
         }
 
-        public static void adjunct (double [,] mat)
+        public static void adjunct (double [][] mat)
         {
-            for (int ii = 0; ii < mat.GetLength(0); ii++)
+            for (int ii = 0; ii < mat.Length; ii++)
             {
-                for (int jj = 0; jj < mat.GetLength(1); jj++)
+                for (int jj = 0; jj < mat[ii].Length; jj++)
                 {
-                    mat[ii,jj] = Math.Pow(-1, (ii+ jj)%2)*mat[ii,jj];
+                    mat[ii][jj] = Math.Pow(-1, (ii+ jj)%2)*mat[ii][jj];
                 }
             }
         }
 
-        public static void multiply (double[,] mat, double value)
+        public static void multiply (double[][] mat, double value)
         {
-            for (int ii = 0; ii < mat.GetLength(0); ii++)
+            for (int ii = 0; ii < mat.Length; ii++)
             {
-                for (int jj = 0; jj < mat.GetLength(1); jj++)
+                for (int jj = 0; jj < mat[ii].Length; jj++)
                 {
-                    mat[ii,jj] = mat[ii,jj]*value;
+                    mat[ii][jj] = mat[ii][jj]*value;
                 }
             }
         }
 
-        public static double[,] copyAround (double[,] mat, double row, double col) //working
+        public static double[][] copyAround (double[][] mat, double row, double col) //working
         {
-            /*
-            for (int ii=0; ii<mat.GetLength(); ii++)
-            {
-                for (int jj=0; jj<mat.GetLength(); jj++)
-                {
-                    Console.Write(mat[ii,jj]+" ");
-                }
-                Console.WriteLine();
-            }*/
-            double[,] newMat = new double[mat.GetLength(0)-1, mat.GetLength(1)-1];
+            double[][] newMat = new double[mat.Length-1][];
 
-            for (int ii=0; ii<newMat.GetLength(0); ii++)
+            for (int ii=0; ii<newMat.Length; ii++)
             {
-                for (int jj=0; jj<newMat.GetLength(1); jj++)
+                newMat[ii] = new double[mat[ii].Length - 1];
+                for (int jj=0; jj<newMat[ii].Length; jj++)
                 {
-                    newMat[ii,jj] = mat[ii < row ? ii : ii + 1,jj<col ? jj :jj+1];
+                    newMat[ii][jj] = mat[ii < row ? ii : ii + 1][jj<col ? jj :jj+1];
                 }
             }
-            /*
-            for (int ii = 0; ii < newMat.GetLength(); ii++)
-            {
-                for (int jj = 0; jj < newMat.GetLength(); jj++)
-                {
-                    Console.Write(newMat[ii,jj] + " ");
-                }
-                Console.WriteLine();
-            }*/
             return newMat;
         }
-        public static void print(double[,] mat)
+        public static void print(double[][] mat)
         {
-            for (int ii = 0; ii < mat.GetLength(0); ii++)
+            for (int ii = 0; ii < mat.Length; ii++)
             {
-                for (int jj = 0; jj < mat.GetLength(1); jj++)
+                for (int jj = 0; jj < mat[ii].Length; jj++)
                 {
-                    Console.Write(mat[ii,jj] + " ");
+                    Console.Write(mat[ii][jj] + " ");
                 }
                 Console.WriteLine();
             }
         }
 
-        public static double[,] multiply(double[,] mat1, double[,] mat2)
+        public static double[][] multiply(double[][] mat1, double[][] mat2)
         {
-            if (mat1.GetLength(1)!=mat2.GetLength(0))
+            if (mat1[0].Length!=mat2.Length)
             {
-                Console.WriteLine(mat1.GetLength(0) + " " + mat2.GetLength(1));
+                Console.WriteLine(mat1.Length + " " + mat2[0].Length);
                 throw new ArgumentException();
             }
-            double[,] result = new double[mat1.GetLength(0), mat2.GetLength(1)];
-            for (int ii=0; ii<mat1.GetLength(0); ii++)
+            double[][] result = new double[mat1.Length][];
+            for (int ii=0; ii<mat1.Length; ii++)
             {
-                for (int jj=0; jj<mat2.GetLength(1); jj++)
+                result[ii] = new double[mat2[ii].Length];
+                for (int jj=0; jj<mat2[ii].Length; jj++)
                 {
                     double total = 0;
-                    for (int kk=0; kk<mat1.GetLength(1); kk++)
+                    for (int kk=0; kk<mat1[ii].Length; kk++)
                     {
-                        total += mat1[ii, kk] * mat2[kk, jj];
+                        total += mat1[ii][kk] * mat2[kk][jj];
                     }
-                    result[ii, jj] = total;
+                    result[ii][ jj] = total;
                 }
             }
             return result;
